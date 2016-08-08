c6e89c3b5c9c33f97e0c31f5dad29a02df4ff12a
Chad Calhoun <chad.m.calhoun@gmail.com>
2016-08-08 11:43:03 -0600

    Resolving issue with nil object in aws sdk adapter

--- lib/dynamoid/adapter_plugin/aws_sdk_v2.rb
+++ lib/dynamoid/adapter_plugin/aws_sdk_v2.rb
@@ -287,6 +287,7 @@ module Dynamoid
       # See: http://docs.aws.amazon.com/sdkforruby/api/Aws/DynamoDB/Client.html#put_item-instance_method
       def put_item(table_name, object, options = {})
         item = {}
+        options = {} if options.nil?
 
         object.each do |k, v|
           next if v.nil? || (v.respond_to?(:empty?) && v.empty?)
